9fedf41a3623a03b5e66448bca5006cb2f2793b1
Love-Becca <follycube2020@gmail.com>
2024-07-27 11:44:36 +0100

    table not rendering

--- src/ui/components/Design.tsx
+++ src/ui/components/Design.tsx
@@ -9,7 +9,7 @@ const Design: React.FC = () => {
                 {tableStyles.map(style => (
                     <div key={style.id} style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '10px', width: '200px', textAlign: 'center' }}>
                         <h3>{style.name}</h3>
-                        <img   src={`.././images/${style.image}`}  alt={style.name} style={{ width: '100%', height: 'auto' }} />
+                        <img   src={`../../images/${style.image}`}  alt={style.name} style={{ width: '100%', height: 'auto' }} />
                         <button style={{ marginTop: '10px' }}>
                             {style.isActive ? 'Active' : 'Select'}
                         </button>
